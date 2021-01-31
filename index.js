a9d77826ab7a1886155feff63edf194800f7ecb7
Satya Sharma <satyasharma2964@gmail.com>
2021-01-31 18:36:03 +0530

    landing Page try 2

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-window.addEventListener("DOMContentLoaded", event => {
-
-  // This is Code is used when audio src is passed in html boilerplate
   const audio = document.querySelector("audio");
   audio.volume = 0.2;
   audio.play();
-})
